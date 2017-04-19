ecba596325964e4f580a8d3e92bbd3b785056507
Brian Chew <bchewaqw@gmail.com>
2017-04-19 21:47:12 +0800

    addtion.py
    addition

---
+++ Addition.py
@@ -0,0 +1,11 @@
+# Ask for input number 1
+# Ask for input number 2
+# Result of addition for both numbers
+# Print final number
+
+no1 = float(input("Number 1:"))
+
+no2 = float(input("Number 2:"))
+
+finalno = no1 + no2
+print(finalno)
