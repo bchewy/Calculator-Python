3e64e198843060d23312cf86f0520fad87d51fe2
Brian Chew <bchewaqw@gmail.com>
2017-04-20 22:38:40 +0800

    Exponentiation (list_easy)

--- Python 3/list_easy.py
+++ Python 3/list_easy.py
@@ -11,9 +11,9 @@
 ## store as float
 no1 = float(input("Number one:"))
 ## create list with different operators
-lst = ['+','-','/','*']
+lst = ['+','-','/','*','**']
 ## print text for humans to understand which operator/how to choose operators
-print("0 = +, 1 = - , 2 = / , 3 = *")
+print("0 = +, 1 = - , 2 = / , 3 = * , 4= **")
 ## declare operator being chosen in lst variable(list)
 op = lst[int(input("Operator:"))]
 ##declare 2nd number to be used on 1st number for calculator
@@ -26,4 +26,5 @@ elif op=="/":
     print("Result:",no1 / no2)
 elif op=="*":
     print("Result:",no1 * no2)
-
+elif op=="**":
+    print("Result:",no1 ** no2)
