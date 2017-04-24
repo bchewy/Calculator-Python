90d1231e4fb4e84ca808bb10f0bfef9dfd8c96af
Brian Chew <bchewaqw@gmail.com>
2017-04-24 10:14:39 +0800

    add new file
    asdasfasd

---
+++ Python 3/casdasd.py
@@ -0,0 +1,30 @@
+# Ask for first number to be used
+# Store first number
+# Print statement to show numbers for different operators
+# Ask for arithmetic operation to be used (+, - ,/ ,* ,**)
+# Ask for second number to be used
+# Store 2nd number
+# Use 2nd number on first number with chosen arithmetic operation
+# Tada!
+
+##Declare 1st number for calculator
+## store as float
+no1 = float(input("Number one:"))
+## create list with different operators
+lst = ['+','-','/','*','**']
+## print text for humans to understand which operator/how to choose operators
+print("0 = +, 1 = - , 2 = / , 3 = * , 4= **")
+## declare operator being chosen in lst variable(list)
+op = lst[int(input("Operator:"))]
+##declare 2nd number to be used on 1st number for calculator
+no2 = float(input("Number two:"))
+if op=="+":
+    print("Result:",no1 + no2)
+elif op=="-":
+    print("Result:",no1 - no2)
+elif op=="/":
+    print("Result:",no1 / no2)
+elif op=="*":
+    print("Result:",no1 * no2)
+elif op=="**":
+    print("Result:",no1 ** no2)
