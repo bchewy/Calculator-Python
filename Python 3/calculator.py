01dbbf49d245e0fe53bdee58c631bb2415f8ee2d
Brian Chew <bchewaqw@gmail.com>
2017-04-30 03:11:26 +0800

    use of boolean to loop (while loop)

---
+++ Python 3/calculator.py
@@ -0,0 +1,11 @@
+on = True
+
+while on == True:
+    line = (input("Type in your equation here: "))
+    print("Answer: ",eval(line))
+'''   try:
+        line == float
+    except TypeError:
+        print("This is not a valid number!")'''
+
+
